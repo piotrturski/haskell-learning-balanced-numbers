1293c024f8b41c116af20bd526aad898140c93f5
Piotr Turski <piotr.turski@gmail.com>
2014-11-13 16:07:26 +0100

    lists replaced with maps

--- Solution.hs
+++ Solution.hs
@@ -9,34 +9,37 @@ import Debug.Trace
 --import Data.Hashable.Class.Hashable
 z = 4
 
-newtype Number_of_digits = Number_of_digits {get_number_of_digits :: Int} deriving (Eq, Num, Show)
+newtype Number_of_digits = Number_of_digits {get_number_of_digits :: Int} deriving (Eq, Num, Show, Integral, Enum, Real, Ord)
 newtype Number_of_numbers = Number_of_numbers {get_number_of_numbers :: Integer} deriving (Eq, Num, Show)
 newtype Sum_of_digits = Sum_of_digits {get_sum_of_digits :: Int} deriving (Num, Enum, Integral, Real, Ord, Eq, Show)
 newtype Sum_of_numbers = Sum_of_numbers {get_sum_of_numbers :: Integer} deriving (Show, Num)
 
-type All_numbers_info = [(Sum_of_digits, Sum_of_numbers)]
+type All_numbers_info = Map Sum_of_digits Sum_of_numbers
 
 {-
 instance Hashable Sum_of_digits where
   hash (Sum_of_digits {get_number_of_digits = a}) = a
   -}
   
-all_numbers_of_size :: Number_of_digits -> All_numbers_info
-all_numbers_of_size 1 = [(x, Sum_of_numbers $ toInteger x) | x <- [0..9]]
-all_numbers_of_size n = 
-	      toList . norm $ [ (Sum_of_digits( sum_of_digits + digit), Sum_of_numbers (sum_of_numbers * 10 + toInteger digit)) |
+all_numbers_of_size :: Number_of_digits -> Int -> All_numbers_info
+all_numbers_of_size 1 from = fromList $ [(x, Sum_of_numbers $ toInteger x) | x <- [0..9]]
+all_numbers_of_size n from = 
+		      fromListWith (+) $ [ (Sum_of_digits( sum_of_digits + digit), Sum_of_numbers (sum_of_numbers * 10 + toInteger digit)) |
 				digit :: Int <- [0..9], 
 				(Sum_of_digits  sum_of_digits, Sum_of_numbers sum_of_numbers) <- smaller_numbers]
 				  where
-				     smaller_numbers = all_numbers_of_size (n-1)
+				     smaller_numbers = toList $ all_numbers_of_size (n-1) from
        
 				
-norm :: All_numbers_info -> Map Sum_of_digits Sum_of_numbers
-norm = fromListWith (+)
+--norm :: All_numbers_info -> Map Sum_of_digits Sum_of_numbers
+norm = undefined
 
 -- TODO middle number
 combine :: All_numbers_info -> All_numbers_info -> Number_of_digits-> Sum_of_numbers
-combine begins ends ends_size = undefined
+combine begins ends ends_size = 
+  sum $
+  elems $
+  intersectionWith (\sum_of_begins sum_of_ends -> 10^ends_size * sum_of_begins + sum_of_ends) begins ends
 
 
 {-
