3707771f6a0a88173f6cd2bfd54454824bfaf80b
Piotr Turski <piotr.turski@gmail.com>
2014-11-15 02:47:01 +0100

    math done. needs cleanup; ~4s to get the answer; probably state monad needed to make it faster

--- Solution.hs
+++ Solution.hs
@@ -7,50 +7,65 @@ import Data.Map
 import Debug.Trace
 --import Data.Hashable
 --import Data.Hashable.Class.Hashable
-z = 4
 
 newtype Number_of_digits = Number_of_digits {get_number_of_digits :: Int} deriving (Eq, Num, Show, Integral, Enum, Real, Ord)
 newtype Number_of_numbers = Number_of_numbers {get_number_of_numbers :: Integer} deriving (Eq, Num, Show)
 newtype Sum_of_digits = Sum_of_digits {get_sum_of_digits :: Int} deriving (Num, Enum, Integral, Real, Ord, Eq, Show)
 newtype Sum_of_numbers = Sum_of_numbers {get_sum_of_numbers :: Integer} deriving (Show, Num, Enum)
 
-type All_numbers_info = Map Sum_of_digits Sum_of_numbers
-
-{-
-instance Hashable Sum_of_digits where
-  hash (Sum_of_digits {get_number_of_digits = a}) = a
-  -}
-  
+type All_numbers_info = Map Sum_of_digits (Sum_of_numbers, Number_of_numbers)
+ 
 all_numbers_of_size :: Number_of_digits -> Int -> All_numbers_info
-all_numbers_of_size 1 from = fromList $ [(Sum_of_digits x, Sum_of_numbers $ toInteger x) | x <- [from..9]]
+all_numbers_of_size 1 from = fromList $ [(Sum_of_digits x, (Sum_of_numbers $ toInteger x, 1)) | x <- [from..9]]
 all_numbers_of_size n from = 
-		      fromListWith (+) $ [ (Sum_of_digits( sum_of_digits + digit), Sum_of_numbers (sum_of_numbers * 10 + toInteger digit)) |
-				digit :: Int <- [0..9], 
-				(Sum_of_digits  sum_of_digits, Sum_of_numbers sum_of_numbers) <- smaller_numbers]
-				  where
-				     smaller_numbers = toList $ all_numbers_of_size (n-1) from
-       
-				
---norm :: All_numbers_info -> Map Sum_of_digits Sum_of_numbers
-norm = undefined
+		      fromListWith pair_of_sums $   
+			[ 
+			  let 
+			      new_sum_of_digits = Sum_of_digits( sum_of_digits + digit)
+			      new_sum_of_numbers = Sum_of_numbers (sum_of_numbers * 10 + toInteger digit * number_of_numbers)
+			      new_number_of_numbers = Number_of_numbers number_of_numbers
+			      in
+			(new_sum_of_digits, (new_sum_of_numbers, new_number_of_numbers))
+			|
+			digit :: Int <- [0..9], 
+			(Sum_of_digits  sum_of_digits, 
+			  (Sum_of_numbers sum_of_numbers, Number_of_numbers number_of_numbers)) 
+			    <- smaller_numbers]
+			where 
+			  smaller_numbers = toList $ all_numbers_of_size (n-1) from
 
--- TODO middle number
+-- http://stackoverflow.com/questions/9722689/haskell-how-to-map-a-tuple
+pair_of_sums (a1,b1) (a2,b2) = (a1+a2, b1+b2)
+
 combine :: All_numbers_info -> All_numbers_info -> Number_of_digits-> Bool -> Sum_of_numbers
-combine begins ends ends_size add_middle_number = 
-  sum $
-  elems $
-  intersectionWith (\sum_of_begins sum_of_ends -> 10^shift_begins_by * sum_of_begins + summand_of_middle_digit + sum_of_ends) begins ends
-  where 
-    shift_begins_by = ends_size + if add_middle_number then 1 else 0
-    summand_of_middle_digit = if add_middle_number then (sum [i * 10 ^ends_size |i <-[0..9]])  else 0
+combine begins ends ends_size add_middle_digit = 
+  sum . elems $ 
+  (intersectionWith (\(Sum_of_numbers sum_of_begins,Number_of_numbers  number_of_begins) 
+                      (Sum_of_numbers sum_of_ends,Number_of_numbers  number_of_ends) -> 
+		Sum_of_numbers (
+		  (10^shift_begins_by * (sum_of_begins * number_of_ends) + (sum_of_ends * number_of_begins) )
+			  -- * 10 +  10^ends_size * (sum [0..9])
+			  * (if add_middle_digit then 10 else 1)
+			  + (summand_of_middle_digit * number_of_begins * number_of_ends)
+			 -- + if add_middle_digit then 10^ends_size * number_of_begins * number_of_ends else 0	
+			  )
+			  --Sum_of_numbers (10^shift_begins_by * sum_of_begins)
+			  
+			  )
+	    begins ends)
+	    --() + summand_of_middle_digit + sum_of_ends) begins ends
+    where 
+	      shift_begins_by = ends_size + if add_middle_digit then 1 else 0
+	      summand_of_middle_digit = if add_middle_digit then (sum [0..9]) * (10 ^ends_size) else 0
 
-sums n = combine (all_numbers_of_size n 1) (all_numbers_of_size n 0) 1 False
+-- sums n = combine (all_numbers_of_size n 1) (all_numbers_of_size n 0) 1 False
 
-{-
-all_numbers_of_size n = foldl add_digits emptyMap [0..9]
-			where
-			  add_digits digitMap digit =
-			  smaller_map = all_numbers_of_size n-1
-			  -}
 
---main = return ()
+t 1 = sum [0..9]
+t n = (combine (all_numbers_of_size half 1) (all_numbers_of_size half 0) half add_middle_digit) + t (n-1)
+	where
+	  (half, remainder) = n `divMod` 2
+	  add_middle_digit = odd remainder
+
+tm n = (get_sum_of_numbers(t n)) `mod`( 3^15)
+	  
--- Tests.hs
+++ Tests.hs
@@ -1,26 +1,19 @@
--- {-# OPTIONS_GHC -F -pgmF htfpp #-}
-
 import Test.HUnit
-import qualified Distribution.TestSuite as Cabal
---import qualified Distribution.TestSuite.HUnit as CabalHUnit
--- import Test.Framework
+import Text.Printf
 import Solution
 
-tests2 = TestList [
-  
-  TestCase $ do 
-	      a <- return 1
-	      let b = 2
-	      assertEqual "first test" a b,
-  TestLabel "with label" $ TestCase (assertEqual "second test" 3 4),
-  TestCase $ assertEqual "3rd test" 5 6,
-  TestCase $ assertEqual "solution test" z 4
- 
-
+tests = TestList [
+     1 --> 45,
+     2 --> 540,
+     3 --> 50040,
+     4 --> 3364890,
+     5 --> 4771029,
+     6 --> 6645504,
+     7 --> 9356034,
+     8 --> 4913436,
+     9 --> 8361561,
+     10 --> 10771919
  ]
+  where n --> value = TestCase $ assertEqual (printf "T(%d)" n) value (tm . Number_of_digits $ n)
 
--- test_nonEmpty = assertEqual 1 2
-
-main = runTestTT tests2
-
---tests = map (\(x,y) -> CabalHUnit.test x y) [("Login tests", tests2)]
+main = runTestTT tests
